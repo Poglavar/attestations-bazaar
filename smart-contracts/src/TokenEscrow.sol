18f887991921f55a825d665893bbf3cbcb9fe1a3
Poglavar Svemira <svemir@nftmagicians.xyz>
2024-03-01 22:55:47 -0700

    Further work on Escrow and Send

--- smart-contracts/src/TokenEscrow.sol
+++ smart-contracts/src/TokenEscrow.sol
@@ -8,23 +8,27 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {SchemaResolver} from "@ethereum-attestation-service/contracts/resolver/SchemaResolver.sol";
 import {IEAS, Attestation} from "@ethereum-attestation-service/contracts/IEAS.sol";
 
-import {ITokenEscrow} from "./ITokenEscrow.sol";
-
-/// @title TokenSender
+/// @title TokenEscrow
 /// @notice A sample schema resolver that checks whether a specific amount of tokens was approved to be included in an attestation.
-contract TokenSender is SchemaResolver {
+contract TokenEscrow is SchemaResolver {
     using SafeERC20 for IERC20;
 
     error InvalidAllowance();
 
     IERC20 private immutable _targetToken;
-    ITokenEscrow private immutable _tokenEscrow;
 
-    constructor(IEAS eas, IERC20 targetToken, ITokenEscrow tokenEscrow) SchemaResolver(eas) {
+    constructor(IEAS eas, IERC20 targetToken) SchemaResolver(eas) {
         _targetToken = targetToken;
-        _tokenEscrow = tokenEscrow;
     }
 
+    // function that can be called to retrieve the target token
+    // only acceptedOfferer can call this function
+    function retrieveToken(uint256 amount) external {
+        _targetToken.safeTransfer(msg.sender, amount);
+    }
+
+    // TODO: at the moment we will take the amount you are pledging every time you call this function,
+    // but in the future why not allow to request with more than the pledged amount?
     function onAttest(Attestation calldata attestation, uint256 /*value*/ ) internal override returns (bool) {
         // if (_targetToken.allowance(attestation.attester, address(this)) < _targetAmount) {
         //     revert InvalidAllowance();
@@ -34,9 +38,8 @@ contract TokenSender is SchemaResolver {
         // Convert it to a uint256
         uint256 amount = uint256(attestation.refUID);
 
-        // send the tokens to the address in the to field
-        // _targetToken.safeTransferFrom(attestation.attester, attestation.recipient, amount);
-        _tokenEscrow.retrieveToken(amount);
+        // send the token to this contract MAKE SURE TO APROVE FIRST
+        _targetToken.safeTransferFrom(attestation.attester, address(this), amount);
 
         return true;
     }
